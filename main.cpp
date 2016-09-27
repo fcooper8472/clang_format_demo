5be2e5ff90d8e134524add43e6fef9ee605b886d
Fergus Cooper <cooper@maths.ox.ac.uk>
2016-09-27 17:39:05 +0100

    Minor updates

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <set>
+
+void PrintSet(std::set<unsigned> mySet);
 
 int main()
 {
     std::cout << "Clang format demmo" << std::endl;
+
+    PrintSet({ 1, 4, 3, 5, 5 });
+
+    return 0;
+}
+
+void PrintSet(std::set<unsigned> mySet)
+{
+    for (auto&& i : mySet)
+    {
+        std::cout << i << std::endl;
+    }
 }
